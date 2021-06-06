931156ec146f14f5955fb39c2c919aacd05184e4
NickOvt <pukpikok1@gmail.com>
2021-06-06 20:45:20 +0300

    Slot machine backend done

--- server/routes/api/casino.js
+++ server/routes/api/casino.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const crypto = require("crypto");
 
 const reel1 = ["cherry", "lemon","apple", "lemon", "banana", "banana", "lemon", "lemon"];
 const reel2 = ["lemon", "apple", "lemon", "lemon", "cherry", "apple", "banana", "lemon"]
@@ -18,27 +17,22 @@ function spin(arrayOfReels) {
   return spinResult;
 }
 
-function removeDuplicates(arr) {
-  return [...new Set(arr)];
-}
-
 function resultOfSpin(spinResult, coins){
   const initialCoins = coins;
 
-  if(spinResult[0] === 'cherry' && spinResult[1] === 'cherry' && spinResult[2] === 'cherry') coins += 50;
-  else if((spinResult[0] === 'cherry' && spinResult[1] === 'cherry') || spinResult[1] === 'cherry' && spinResult[2] === 'cherry') coins += 40;
-  else if(spinResult[0] === 'apple' && spinResult[1] === 'apple' && spinResult[2] === 'apple') coins += 20;
-  else if((spinResult[0] === 'apple' && spinResult[1] === 'apple') || spinResult[1] === 'apple' && spinResult[2] === 'apple') coins += 10;
-  else if(spinResult[0] === 'banana' && spinResult[1] === 'banana' && spinResult[2] === 'banana') coins += 15;
-  else if((spinResult[0] === 'banana' && spinResult[1] === 'banana') || spinResult[1] === 'banana' && spinResult[2] === 'banana') coins += 5;
-  else if(spinResult[0] === 'lemon' && spinResult[1] === 'lemon' && spinResult[2] === 'lemon') coins += 3;
+  if(spinResult[0] == 'cherry' && spinResult[1] == 'cherry' && spinResult[2] == 'cherry') coins += 50;
+  else if((spinResult[0] == 'cherry' && spinResult[1] == 'cherry') || spinResult[1] == 'cherry' && spinResult[2] == 'cherry') coins += 40;
+  else if(spinResult[0] == 'apple' && spinResult[1] == 'apple' && spinResult[2] == 'apple') coins += 20;
+  else if((spinResult[0] == 'apple' && spinResult[1] == 'apple') || spinResult[1] == 'apple' && spinResult[2] == 'apple') coins += 10;
+  else if(spinResult[0] == 'banana' && spinResult[1] == 'banana' && spinResult[2] == 'banana') coins += 15;
+  else if((spinResult[0] == 'banana' && spinResult[1] == 'banana') || spinResult[1] == 'banana' && spinResult[2] == 'banana') coins += 5;
+  else if(spinResult[0] == 'lemon' && spinResult[1] == 'lemon' && spinResult[2] == 'lemon') coins += 3;
   
-
-
   return {
     spinResult,
     coins,
-    isWin: initialCoins <= coins ? true : false
+    coinsWon: coins - initialCoins,
+    isWin: initialCoins < coins ? true : false
   }
 }
 
