9545d9c3ec61a184aa4442661ef1d926c4e6b23b
NickOvt <pukpikok1@gmail.com>
2021-06-07 18:15:53 +0300

    Frontend add comments and typo fix

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/authActions';
 
+// When the app/ website is started get the token from the localStorage and load the user(if there is token)
 function App() {
   useEffect(() => {
     store.dispatch(loadUser());
